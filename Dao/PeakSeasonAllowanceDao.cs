d272158621111408c1729410265ba369729efd47
辻祐一 <yuuichi.t@outlook.com>
2026-07-31 11:00:52 +0900

    EGov以外をバックアップ

--- Dao/PeakSeasonAllowanceDao.cs
+++ Dao/PeakSeasonAllowanceDao.cs
@@ -25,6 +25,13 @@ namespace Dao {
 
         /// <summary>
         /// 繁忙期割り増し費 対象者集計表用
+        /// 
+        /// 条件定義
+        /// ①指定した期間内
+        /// ②H_StaffMaster.Belongs IN (12, 22)
+        /// ③H_StaffMaster.JobForm IN (20, 22, 99)
+        /// ④-1 VehicleDispatchDetail.StaffOccupation = 11 作業員
+        /// ④-2 VehicleDispatchDetail.StaffOccupation = 10 AND (H_CarMaster.CarKindCode = 10 OR (H_CarMaster.CarKindCode = 11 AND H_CarMaster.ShapeCode = 10)) 運転手 AND 軽自動車 OR (小型ANDキャブオーバー)
         /// </summary>
         /// <param name="operationDate1"></param>
         /// <param name="operationDate2"></param>
@@ -33,37 +40,36 @@ namespace Dao {
             List<PeakSeasonAllowanceVo> listPeakSeasonAllowanceVo = new ();
 
             SqlCommand sqlCommand = _connectionVo.SqlServerConnection.CreateCommand();
-            sqlCommand.CommandText =
-                "SELECT VehicleDispatchDetail.StaffCode, " +
-                "       H_StaffMaster.UnionCode, " +
-                "       H_BelongsMaster.Name AS BelongsName, " +
-                "       H_StaffMaster.DisplayName, " +
-                "       COUNT(*) AS HitCount " +
-                "FROM (SELECT StaffCode1 AS StaffCode, StaffOccupation1 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
-                "      FROM H_VehicleDispatchDetail " +
-                "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND (ClassificationCode = 10 OR ClassificationCode = 11 OR ClassificationCode = 12 OR ClassificationCode = 30) " +
-                "UNION ALL " +
-                "      SELECT StaffCode2 AS StaffCode, StaffOccupation2 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
-                "      FROM H_VehicleDispatchDetail " +
-                "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND (ClassificationCode = 10 OR ClassificationCode = 11 OR ClassificationCode = 12 OR ClassificationCode = 30) " +
-                "UNION ALL " +
-                "      SELECT StaffCode3 AS StaffCode, StaffOccupation3 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
-                "      FROM H_VehicleDispatchDetail " +
-                "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND (ClassificationCode = 10 OR ClassificationCode = 11 OR ClassificationCode = 12 OR ClassificationCode = 30) " +
-                "UNION ALL " +
-                "      SELECT StaffCode4 AS StaffCode, StaffOccupation4 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
-                "      FROM H_VehicleDispatchDetail " +
-                "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND (ClassificationCode = 10 OR ClassificationCode = 11 OR ClassificationCode = 12 OR ClassificationCode = 30)) AS VehicleDispatchDetail " +
-                "INNER JOIN H_StaffMaster ON VehicleDispatchDetail.StaffCode = H_StaffMaster.StaffCode " +
-                "LEFT JOIN H_CarMaster ON VehicleDispatchDetail.CarCode = H_CarMaster.CarCode " +
-                "LEFT JOIN H_BelongsMaster ON H_StaffMaster.Belongs = H_BelongsMaster.Code " +
-                "WHERE VehicleDispatchDetail.OperationDate BETWEEN @OperationDate1 AND @OperationDate2 " +
-                  "AND (H_StaffMaster.Belongs = 12 OR H_StaffMaster.Belongs = 22) " +
-                  "AND (H_StaffMaster.JobForm = 20 OR H_StaffMaster.JobForm = 22 OR H_StaffMaster.JobForm = 99) " +
-                  "AND (VehicleDispatchDetail.StaffOccupation = 11 OR (VehicleDispatchDetail.StaffOccupation = 10 AND (H_CarMaster.CarKindCode = 10 OR (H_CarMaster.CarKindCode = 11 AND H_CarMaster.ShapeCode = 10)))) " +
-                  "AND DATEPART(WEEKDAY, VehicleDispatchDetail.OperationDate) <> 1 " +   // ★ 日曜日除外
-                  "GROUP BY VehicleDispatchDetail.StaffCode, H_StaffMaster.UnionCode, H_StaffMaster.DisplayName, H_BelongsMaster.Name " +
-                  "ORDER BY H_StaffMaster.UnionCode";
+            sqlCommand.CommandText = "SELECT VehicleDispatchDetail.StaffCode, " +
+                                     "       H_StaffMaster.UnionCode, " +
+                                     "       H_BelongsMaster.Name AS BelongsName, " +
+                                     "       H_StaffMaster.DisplayName, " +
+                                     "       COUNT(*) AS HitCount " +
+                                     "FROM (SELECT StaffCode1 AS StaffCode, StaffOccupation1 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
+                                     "      FROM H_VehicleDispatchDetail " +
+                                     "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND ClassificationCode IN (10, 11, 12, 30) " +
+                                     "UNION ALL " +
+                                     "      SELECT StaffCode2 AS StaffCode, StaffOccupation2 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
+                                     "      FROM H_VehicleDispatchDetail " +
+                                     "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND ClassificationCode IN (10, 11, 12, 30) " +
+                                     "UNION ALL " +
+                                     "      SELECT StaffCode3 AS StaffCode, StaffOccupation3 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
+                                     "      FROM H_VehicleDispatchDetail " +
+                                     "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND ClassificationCode IN (10, 11, 12, 30) " +
+                                     "UNION ALL " +
+                                     "      SELECT StaffCode4 AS StaffCode, StaffOccupation4 AS StaffOccupation, OperationDate, CarCode, ClassificationCode " +
+                                     "      FROM H_VehicleDispatchDetail " +
+                                     "      WHERE OperationFlag = 1 AND VehicleDispatchFlag = 1 AND ClassificationCode IN (10, 11, 12, 30)) AS VehicleDispatchDetail " +
+                                     "INNER JOIN H_StaffMaster ON VehicleDispatchDetail.StaffCode = H_StaffMaster.StaffCode " +
+                                     "LEFT JOIN H_CarMaster ON VehicleDispatchDetail.CarCode = H_CarMaster.CarCode " +
+                                     "LEFT JOIN H_BelongsMaster ON H_StaffMaster.Belongs = H_BelongsMaster.Code " +
+                                     "WHERE VehicleDispatchDetail.OperationDate BETWEEN @OperationDate1 AND @OperationDate2 " +
+                                     "  AND H_StaffMaster.Belongs IN (12, 22) " +
+                                     "  AND H_StaffMaster.JobForm IN (20, 22, 99) " +
+                                     "  AND (VehicleDispatchDetail.StaffOccupation = 11 OR (VehicleDispatchDetail.StaffOccupation = 10 AND (H_CarMaster.CarKindCode = 10 OR (H_CarMaster.CarKindCode = 11 AND H_CarMaster.ShapeCode = 10)))) " +
+                                     "  AND DATENAME(WEEKDAY, VehicleDispatchDetail.OperationDate) <> 'Sunday' " +
+                                     "GROUP BY VehicleDispatchDetail.StaffCode, H_StaffMaster.UnionCode, H_StaffMaster.DisplayName, H_BelongsMaster.Name " +
+                                     "ORDER BY H_StaffMaster.UnionCode";
 
             sqlCommand.Parameters.Add("@OperationDate1", SqlDbType.Date).Value = operationDate1.Date;
             sqlCommand.Parameters.Add("@OperationDate2", SqlDbType.Date).Value = operationDate2.Date;
